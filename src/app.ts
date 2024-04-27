70175735ff13ef6ead7ac33ea1d9660c0d599f48
kizunas <8s6s7e@gmail.com>
2024-04-27 15:37:41 +0900

    Generic関数

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,21 @@
 const names: Array<string> = [];
 // names[0].split(' ');
 
-const promise: Promise<string> = new Promise((resolve, reject) => { // promiseはnew promiseで作成できるJavaScriptのコンストラクタ関数。resolve, rejectを引数に持つ
-  setTimeout(() => {
-    resolve('終わりました!');
-  }, 2000);
-})
+// const promise: Promise<string> = new Promise((resolve, reject) => { // promiseはnew promiseで作成できるJavaScriptのコンストラクタ関数。resolve, rejectを引数に持つ
+//   setTimeout(() => {
+//     resolve('終わりました!');
+//   }, 2000);
+// })
 
-promise.then(data => {
-  data.split(' ');
-})
+// promise.then(data => {
+//   data.split(' ');
+// })
+
+// 独自のGenerics型
+function merge<T extends {}, U>(objA: T, objB: U) { // 一つ目の引数をT,二つ目をUとするのが慣例的らしい。引数の値から型を動的に推論してくれる。
+  return Object.assign(objA, objB);
+}
+
+// console.log(merge({name: 'Max'}, {age:30}))
+const margedObj = merge({name: 'Max'}, {age:30})
+margedObj.age;
